cdb99be7ed383b3d18adcbe8faa171c0e0bba357
Nuh Ayvaz <manahter@gmail.com>
2023-08-12 23:15:58 +0300

    Woodmart Theme Clone Created

---
+++ README.md
@@ -0,0 +1,5 @@
+## Woodmart Theme - Demo
+
+
+<video src="https://github.com/manahter/woodmart-demo/assets/73780835/4fe9169f-aae4-4c2c-850d-b76c39d9beb5" controls="controls" style="max-width: 730px;">
+</video>
---
+++ js/hover-tabs.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function(){
+    $('.nav-tabs > li > a').hover(function() {
+          $(this).tab('show');
+    });
+    $('.nav-tabs-carousel > li > a').hover(function() {
+          $(this).tab('show');
+    });
+})
